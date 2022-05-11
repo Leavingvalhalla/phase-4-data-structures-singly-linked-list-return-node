1494bce19734c84eea37625a713b1f1bfcf920de
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-11 14:30:41 -0700

    commit tree 8e62330f7fea0de7cf71b6c48e44073bdf62edef

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,26 @@ class LinkedList
     @head = nil
   end
 
+  def size
+    return 0 if @head == nil
+    last = @head
+    count = 1
+    while last.next_node
+      last = last.next_node
+      count +=1
+    end
+    count
+  end
+
   def nth_from_end(n)
-    # your code here
+    return nil if n > self.size || n < 1 
+    target = @head
+    (self.size - n).times do
+    target = target.next_node
+    end
+  target.value
   end
 
 end
+
+
